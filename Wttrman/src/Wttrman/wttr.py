b225bdb7aecdfa7fcf66930b0978d8620e839207
Melvin Quick <melvinquick@proton.me>
2025-02-19 14:42:59 -0500

    Removed superfluous code and generalized the remaining text_fixer function.

--- Wttrman/src/Wttrman/wttr.py
+++ Wttrman/src/Wttrman/wttr.py
@@ -6,39 +6,20 @@ class Wttr:
         self.base_url = "https://wttr.in/"
 
     def get_weather(self, city="", state="", country="USA"):
-        city = self.city_fixer(city)
-        state = self.state_fixer(state)
+        city = self.text_fixer(city)
+        state = self.text_fixer(state)
         output_format = r"?format=Condition:+%C+\nPrecip:+%p/3 hours+\nTemps:+%t(%f)"
         url = f"{self.base_url}{city}+{state}+{country}{output_format}"
         weather = get(url, timeout=5)
         return weather.text
 
-    def city_fixer(self, city=""):
+    def text_fixer(self, text=""):
         counter = 0
-        city = list(city)
-        for character in city:
-            if city[counter] == " ":
-                city[counter] = "+"
+        text = list(text)
+        for character in text:
+            if text[counter] == " ":
+                text[counter] = "+"
             counter += 1
 
-        return "".join(city)
+        return "".join(text)
 
-    def state_fixer(self, state=""):
-        counter = 0
-        state = list(state)
-        for character in state:
-            if state[counter] == " ":
-                state[counter] = "+"
-            counter += 1
-
-        return "".join(state)
-
-    def country_fixer(self, country=""):
-        counter = 0
-        country = list(country)
-        for character in country:
-            if country[counter] == " ":
-                country[counter] = "+"
-            counter += 1
-
-        return "".join(country)
