18f1333889251271aeb1da2f40277080b7fb2fec
melvinquick <melvinquick@proton.me>
2025-10-31 15:02:23 -0400

    Bring required python version down to 3.10

--- install.py
+++ install.py
@@ -99,7 +99,7 @@ def install():
     venv_path = get_venv_path()
     create_venv(venv_path)
     install_app(venv_path)
-    version = "1.2.2"
+    version = "1.2.3"
     icon = get_icon(venv_path)
     python = get_python_path(venv_path)
     app = get_app_path(venv_path)
